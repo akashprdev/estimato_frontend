8e5d1ab23ed813be73c28870f32038780e27161d
Akash Pradhan <akashpradhan151@gmail.com>
2026-04-02 21:58:36 +0530

    serve url from env

--- src/utilities/socket.ts
+++ src/utilities/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:8080', {
+export const socket = io(import.meta.env.VITE_API_URL as string, {
   autoConnect: false,
   transports: ['websocket'],
 });
