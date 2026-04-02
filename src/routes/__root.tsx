b902b27a7412241fe184e1f186026274f32a39a6
Akash Pradhan <akashpradhan151@gmail.com>
2026-04-02 18:00:17 +0530

    added favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -18,7 +18,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Vibely',
+        title: 'Estimato',
       },
     ],
     links: [
